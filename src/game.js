2044c66976fb0b6b549a273bbc93ba3a5a4573c6
hayesmaker <andygoestohollywood@gmail.com>
2018-02-06 23:47:45 +0000

    implemented tests + game

--- src/game.js
+++ src/game.js
@@ -1,63 +1,100 @@
 module.exports = Game;
 
 function Game() {
-  this.grid = [];
+  this.grid = this.eraseGrid();
+  this.newGrid = this.eraseGrid();
   this.generation = 0;
-  this.addRow();
-  this.addRow();
-  this.addRow();
-  this.addRow();
-  this.addRow();
-  this.addRow();
-  this.addRow();
-  this.addRow();
-  this.addRow();
-  this.addRow();
-  this.logGrid();
 }
 
-//var p = Game.prototype;
+var p = Game.prototype;
 
-Game.grid = null;
+p.grid = null;
 
-Game.prototype.addRow = function() {
-  this.grid.push([0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+p.eraseGrid = function () {
+  return [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+  ];
 };
 
-Game.prototype.spawnCell = function (row, col) {
+p.spawnCell = function (row, col) {
   this.grid[row][col] = 1;
 };
 
-Game.prototype.killCell = function (row, col) {
+p.killCell = function (row, col) {
   this.grid[row][col] = 0;
 };
 
-Game.prototype.detectNumNeighbours = function(row, col) {
+p.detectNumNeighbours = function (row, col) {
   var numNeighbours = 0;
-  numNeighbours += this.grid[row-1][col];
-  numNeighbours += this.grid[row+1][col];
-  numNeighbours += this.grid[row][col-1];
-  numNeighbours += this.grid[row][col+1];
+  //top left diagonal
+  if (this.grid[row -1] && this.grid[row -1][col - 1]) {
+    numNeighbours += this.grid[row - 1][col - 1];
+  }
+  //top right diagonal
+  if (this.grid[row -1] && this.grid[row -1][col + 1]) {
+    numNeighbours += this.grid[row - 1][col + 1];
+  }
+  //bottom left diagonal
+  if (this.grid[row + 1] && this.grid[row +1][col - 1]) {
+    numNeighbours += this.grid[row + 1][col - 1];
+  }
+  //bottom right diagonal
+  if (this.grid[row + 1] && this.grid[row + 1][col + 1]) {
+    numNeighbours += this.grid[row + 1][col + 1];
+  }
+  numNeighbours += this.grid[row - 1] ? this.grid[row - 1][col] : 0;          //left
+  numNeighbours += this.grid[row + 1] ? this.grid[row + 1][col] : 0;          //right
+  numNeighbours += this.grid[row][col - 1] ? this.grid[row][col - 1] : 0;     //top
+  numNeighbours += this.grid[row][col + 1] ? this.grid[row][col + 1] : 0;     //bottom
   return numNeighbours;
 };
 
-
-Game.prototype.gameLoop = function() {
+/**
+ * Main Game Loop Steps forwards 1 generation
+ * First this method checks the game of life rules every square of the grid, and updates a new grid based on the result
+ * Then the new grid is copied to the main grid model
+ * Then the new gris is cleared ready for the next gen.
+ *
+ * @method gameLoop
+ */
+p.gameLoop = function () {
   this.generation++;
-  //rule 1
-
-  //rule 2
-
-  //rule 3
-
-  //rule 4
-
+  var i, j;
+  for (i = 0; i < this.grid.length; i++) {
+    for (j = 0; j < this.grid[i].length; j++) {
+      this.newGrid[i][j] = this.checkRules(i, j);
+    }
+  }
+  this.grid = this.newGrid.slice();
+  this.newGrid = this.eraseGrid();
+  this.render();
 };
 
-Game.prototype.logGrid = function() {
+p.checkRules = function (row, col) {
+  if (this.detectNumNeighbours(row, col) < 2) {          //1 death from under population
+    return 0;
+  } else if (this.detectNumNeighbours(row, col) > 3) {   //3: death over population
+    return 0;
+  } else {
+    return 1;                                            //2 & 4: stay alive or spawned through reproduction
+  }
+};
+
+p.render = function () {
   var i;
   var grid = this.grid;
-  for (i = 0; i<grid.length; i++) {
+  console.log('--------------------------');
+  for (i = 0; i < grid.length; i++) {
     console.log(i, grid[i]);
   }
+  console.log('--------------------------');
 };
--- test/test.js
+++ test/test.js
@@ -6,11 +6,11 @@ var expect = chai.expect;
 describe('Game Of Life', function() {
   var game;
 
-  before(function(){
+  beforeEach(function(){
     game = new Game();
   });
 
-  after(function() {
+  afterEach(function() {
     game = null;
   });
 
@@ -20,6 +20,7 @@ describe('Game Of Life', function() {
 
   it('should create 10 columns', function() {
     var i;
+    expect(game.grid.length).to.be.above(0);
     for (i = 0; i<game.grid.length; i++) {
         expect(game.grid[i].length).to.equal(10);
     }
@@ -45,36 +46,145 @@ describe('Game Of Life', function() {
     expect(game.grid[2][5]).to.equal(0);
   });
 
-  it('given 1 seed at 2,5, should count 0 live neighbours', function() {
-    game.spawnCell(2,5);
+  describe('Counting neighbours', function() {
+    it('given 1 seed at 2,5, should count 0 live neighbours', function() {
+      game.spawnCell(2,5);
 
-    var numNeighbours = game.detectNumNeighbours(2, 5);
-    expect(numNeighbours).to.equal(0);
+      var numNeighbours = game.detectNumNeighbours(2, 5);
+      expect(numNeighbours).to.equal(0);
+    });
+
+    it('given 2 cells spawn next to seed, should count 2 live neighbours', function() {
+      game.spawnCell(2,5);
+
+      game.spawnCell(1, 5);
+      game.spawnCell(3, 5);
+
+      var numNeighbours = game.detectNumNeighbours(2, 5);
+      expect(numNeighbours).to.equal(2);
+    });
+
+    it('given 1 seed at 3,5, and 2 neighbours, should count 2 live neighbours', function() {
+      game.spawnCell(2,5);
+      game.spawnCell(3,5);
+      game.spawnCell(4,5);
+      var numNeighbours = game.detectNumNeighbours(3, 5);
+      expect(numNeighbours).to.equal(2);
+    });
+
+    it('given 1 seed at 3,5, and 4 diagonal neighbours, should count 4 live neighbours', function() {
+      game.spawnCell(2,4);
+      game.spawnCell(3,5);
+      game.spawnCell(2,6);
+      game.spawnCell(4,4);
+      game.spawnCell(4,6);
+      var numNeighbours = game.detectNumNeighbours(3, 5);
+      expect(numNeighbours).to.equal(4);
+    });
+
+    it('given 1 seed at the edge of the game grid, shouldn\'t error', function() {
+      game.spawnCell(0,0);
+      var numNeighbours = game.detectNumNeighbours(0, 0);
+      expect(numNeighbours).to.equal(0);
+
+    });
   });
 
-  it('given 2 cells spawn next to seed, should count 2 live neighbours', function() {
-    game.spawnCell(2,5);
 
-    game.spawnCell(1, 5);
-    game.spawnCell(3, 5);
-
-    var numNeighbours = game.detectNumNeighbours(2, 5);
-    expect(numNeighbours).to.equal(2);
-  });
 
 
   describe('Rule 1: Any live cell with fewer than two live neighbours dies, as if caused by underpopulation', function() {
 
     it('given only 1 seed at 3,5, the cell should die', function() {
-
       game.spawnCell(3,5);
-      game.gameLoop();
-      expect(game.grid[3][5]).to.equal(0);
+      var cellState = game.checkRules(3,5);
+      expect(cellState).to.equal(0);
+    });
+
+  });
+
+
+  describe('Rule 2: Any live cell with two or three live neighbours lives on to the next generation.', function() {
+
+    it('given 1 seed at 3,5 and 2 neighbours the cell should live', function() {
+      game.spawnCell(2,5);
+      game.spawnCell(3,5);
+      game.spawnCell(4,5);
+      var cellState = game.checkRules(3,5);
+      expect(cellState).to.equal(1);
+    });
+
+    it('given 1 seed at 3,5 and 3 neighbours the cell should live', function() {
+      game.spawnCell(2,5);
+      game.spawnCell(3,5);
+      game.spawnCell(3,4);
+      game.spawnCell(3,6);
+      var cellState = game.checkRules(3,5);
+      expect(cellState).to.equal(1);
+    });
+
+  });
+
+  describe('Rule 3: Any live cell with more than three live neighbours dies, as if by overpopulation.', function() {
+
+    it('given 1 seed at 3,5 and 4 neighbours the cell should die', function() {
+      game.spawnCell(3,5);
+      game.spawnCell(2,5);
+      game.spawnCell(4,5);
+      game.spawnCell(3,6);
+      game.spawnCell(3,4);
+      var cellState = game.checkRules(3,5);
+      expect(cellState).to.equal(0);
     });
 
 
   });
 
 
+  describe('Rule 4: Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.', function() {
+
+    it('given a dead cell at 3,5 and 3 neighbours the cell should spawn', function() {
+      game.spawnCell(2,5);
+      game.spawnCell(4,5);
+      game.spawnCell(3,6);
+      var cellState = game.checkRules(3,5);
+      expect(cellState).to.equal(1);
+    });
+
+
+  });
+
+  describe('GameLoop', function() {
+    it('render am empty game without error', function() {
+      game.gameLoop();
+    });
+
+    it('render a generation with seeds without error', function() {
+      game.spawnCell(3,5);
+      game.spawnCell(2,5);
+      game.spawnCell(4,5);
+      game.gameLoop();
+    });
+
+    it('render 10 generations with seeds without error', function() {
+      game.spawnCell(3,5);
+      game.spawnCell(2,5);
+      game.spawnCell(4,5);
+      game.gameLoop();
+      game.gameLoop();
+      game.gameLoop();
+      game.gameLoop();
+      game.gameLoop();
+      game.gameLoop();
+      game.gameLoop();
+      game.gameLoop();
+      game.gameLoop();
+      game.gameLoop();
+      expect(game.generation).to.equal(10);
+    });
+  });
+
+
 
 });
+
